46767ec7e0c09865010c5346264397cffca363c7
Thamsen <thomas@bzn.no>
2026-01-15 10:15:49 +0100

    Første versjon. Leser fil. Legger til crate csv.

--- project1/app/src/main.rs
+++ project1/app/src/main.rs
@@ -1,3 +1,7 @@
+use std::{error::Error, fs::File};
+
+use csv::StringRecord;
+
 #[derive(Debug)]
 struct Item {
     i: usize,
@@ -5,7 +9,7 @@ struct Item {
     w: usize,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     
     // Read data from project1/knapsack/knapPI_12_500_1000_82.csv
     // The data format is I(ndex), p(rofit), w(eight)
@@ -14,11 +18,37 @@ fn main() {
     // - the profit is highest, given that 
     // - the total weight of items don't exceed 280785
 
-    let item = Item { 
-        i: 1,
-        p: 2,
-        w: 3
-    };
 
-    println!("{:?}", item)
+    let file = "knapsack/knapPI_12_500_1000_82.csv".to_string();
+
+    let items: Vec<Item> = read_from_file(&file)?;
+    if items.is_empty() {
+        return Err(format!("No items read from file {file}").into());
+    }
+
+    println!("{:?}", items[0]);
+
+    Ok(())
+}
+
+fn read_from_file(path: &String) -> Result<Vec<Item>, Box<dyn Error>> {
+    println!("Reading file...");
+    let file = File::open(path)?;
+
+    let mut readerbuilder = csv::ReaderBuilder::new()
+        .has_headers(true)
+        .from_reader(file);
+
+    let mut items = Vec::new();
+    for record in readerbuilder.records() {
+        let r: StringRecord = record?;
+        let i: usize = r[0].trim().parse().unwrap();
+        let p: usize = r[1].trim().parse().unwrap();
+        let w: usize = r[2].trim().parse().unwrap();
+
+        items.push(Item {i, p, w});
+    
+    }
+
+    Ok(items)
 }
