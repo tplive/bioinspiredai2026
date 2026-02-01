08fae237025c85bd38da1cbb7ec9af9cca6069a1
Thamsen <thomas@bzn.no>
2026-02-01 16:26:43 +0100

    Calculate RMSE for a single generation

--- part2/src/ga.rs
+++ part2/src/ga.rs
@@ -1,9 +1,10 @@
-use crate::chromosome::Chromosome;
+use crate::{chromosome::Chromosome, fitness_evaluator::FitnessEvaluator};
 
 
 pub struct GeneticAlgorithm {
     pub population_size: usize,
     pub num_features: usize,
+    pub evaluator: FitnessEvaluator,
 }
 
 impl GeneticAlgorithm {
@@ -13,6 +14,31 @@ impl GeneticAlgorithm {
             .map(|_| Chromosome::new(self.num_features))
             .collect();
         
-        (Chromosome::new(3), vec![0.1, 0.2, 0.55])
+        self.evaluate_population(&mut population);
+
+        let mut best_fitness_history: Vec<f64> = Vec::new();
+
+        // TODO Run over generations
+
+        let best = population.iter()
+            .min_by(|a, b| {
+                a.fitness.unwrap().partial_cmp(&b.fitness.unwrap()).unwrap()
+            })
+            .unwrap()
+            .clone();
+
+        best_fitness_history.push(best.fitness.unwrap());
+
+        println!("Generation {}: Best RMSE = {:?}", 0, best.fitness.unwrap());
+        
+        (best, best_fitness_history)
+    }
+    
+    fn evaluate_population(&self, population: &mut [Chromosome]) {
+        for c in population.iter_mut() {
+            if c.fitness.is_none() {
+                c.fitness = Some(self.evaluator.evaluate(c));
+            }
+        }
     }
 }
